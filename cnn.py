79a07f87daca959749b7d0d24f642c1914b6af59
wei <1352241452@qq.com>
2017-03-08 17:40:34 +0800

    add data to ignore and get data runnable

--- cnn.py
+++ cnn.py
@@ -1,28 +1,12 @@
-# -*- coding:utf-8 -*-
+import torch
 import torchvision
 import torchvision.transforms as transforms
-import torch
-from torch.autograd import Variable
+import torch.nn as nn
+import torch.optim as optim
 import matplotlib.pyplot as plt
 import numpy as np
-from torch import optim
-import torch.nn as nn
 import torch.nn.functional as F
-
-# data loader, network(define structure and overwrite forward method),
-# criterion(output, labels), optimizer(parameter)
-# process of training:
-optimizer.zero_grad()
-
-# forward + backward + optimize
-# for every batch:
-# outputs = net(inputs)
-# loss = criterion(outputs, labels)
-# loss.backward()
-# optimizer.step()
-
-
-
+from torch.autograd import Variable
 
 
 # The output of torchvision datasets are PILImage images of range [0, 1].
@@ -30,11 +14,11 @@ optimizer.zero_grad()
 transform=transforms.Compose([transforms.ToTensor(),
                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                              ])
-trainset = torchvision.datasets.CIFAR10(root='./documents/data/', train=True, download=True, transform=transform)
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                           shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./documents/data', train=False, download=True, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                           shuffle=False, num_workers=2)
 classes = ('plane', 'car', 'bird', 'cat',
@@ -47,6 +31,7 @@ def imshow(img):
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1,2,0)))
 
+# show some random training images
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
 
@@ -55,39 +40,32 @@ imshow(torchvision.utils.make_grid(images))
 # print labels
 print(' '.join('%5s'%classes[labels[j]] for j in range(4)))
 
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)  # 1 input image channel, 6 output channels, 5x5 square convolution kernel
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool  = nn.MaxPool2d(2,2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # an affine operation: y = Wx + b
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1   = nn.Linear(16*5*5, 120)
+        self.fc2   = nn.Linear(120, 84)
+        self.fc3   = nn.Linear(84, 10)
 
     def forward(self, x):
-        # 返回的是x,x是一个tensor variable,在函数返回后，其creator中储存了全部的运算过程
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))  # Max pooling over a (2, 2) window
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)  # If the size is a square you can only specify a single number
-        x = x.view(-1, self.num_flat_features(x))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16*5*5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
-
-
 net = Net()
-# set criterion and optimizer
+
+
+
 criterion = nn.CrossEntropyLoss() # use a Classification Cross-Entropy loss
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-# train the network
+
 for epoch in range(2):  # loop over the dataset multiple times
 
     running_loss = 0.0
@@ -121,16 +99,16 @@ images, labels = dataiter.next()
 imshow(torchvision.utils.make_grid(images))
 print('GroundTruth: ', ' '.join('%5s'%classes[labels[j]] for j in range(4)))
 
-correct = 0
-total = 0
-for data in testloader:
-    images, labels = data
-    outputs = net(Variable(images))
-    _, predicted = torch.max(outputs.data, 1)
-    total += labels.size(0)
-    correct += (predicted == labels).sum()
+outputs = net(Variable(images))
 
-print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+# the outputs are energies for the 10 classes.
+# Higher the energy for a class, the more the network
+# thinks that the image is of the particular class
+
+# So, let's get the index of the highest energy
+_, predicted = torch.max(outputs.data, 1)
+
+print('Predicted: ', ' '.join('%5s'% classes[predicted[j][0]] for j in range(4)))
 
 class_correct = list(0. for i in range(10))
 class_total = list(0. for i in range(10))
